9f9b6b63a27e0b5684a51fb32290c88d8ddab4c4
Denis Knauf <Denis.Knauf@gmail.com>
2010-03-25 10:39:30 +0100

    Inc: files moved and splitted

---
+++ lib/logan/inc/command.rb
@@ -0,0 +1,40 @@
+
+module Logan
+	module Inc
+		class Command < ::Array
+			attr_reader :sid
+			def initialize sid = 0
+				@sid = sid
+			end
+
+			def event_read line, sock
+				cmd, l = line.unpack 'na*'
+				self[cmd].call( l, sock)  if self[cmd]
+			end
+		end
+
+		class SID0 < Command
+			def initialize store, config, sid = 0
+				@store, @config = store, config
+				self[9] = method :event_hostname
+				self[10] = method :event_filerotated
+			end
+
+			def event_filerotated line, sock
+				sid, d = line.unpack 'Na8'
+				@store[ :seeks, sid] = d
+			end
+
+			def event_hostname line, sock
+				@config[ :hosts].each do |sid,host|
+					next  unless line == host
+					file = @config[ :files, sid]
+					next  unless file
+					# command, SID, (inode, seek), file
+					pc = [1, sid, @store[ :seeks, sid] || "\x00\x00\x00\x00\x00\x00\x00\x00", file].pack 'nNa8a*'
+					sock.write [pc.length, pc].pack( 'Na*')
+				end
+			end
+		end
+	end
+end
--- lib/logan/inc/fileparser.rb
+++ lib/logan/inc/fileparser.rb
@@ -1,6 +1,4 @@
 
-require 'select'
-
 module Logan
 	module Inc
 		module FileParser
@@ -55,43 +53,6 @@ module Logan
 						else @linebuffer += line
 						end
 					end
-				end
-			end
-
-		end
-
-		class Command < ::Array
-			attr_reader :sid
-			def initialize sid = 0
-				@sid = sid
-			end
-
-			def event_read line, sock
-				cmd, l = line.unpack 'na*'
-				self[cmd].call( l, sock)  if self[cmd]
-			end
-		end
-
-		class SID0 < Command
-			def initialize store, config, sid = 0
-				@store, @config = store, config
-				self[9] = method :event_hostname
-				self[10] = method :event_filerotated
-			end
-
-			def event_filerotated line, sock
-				sid, d = line.unpack 'Na8'
-				@store[ :seeks, sid] = d
-			end
-
-			def event_hostname line, sock
-				@config[ :hosts].each do |sid,host|
-					next  unless line == host
-					file = @config[ :files, sid]
-					next  unless file
-					# command, SID, (inode, seek), file
-					pc = [1, sid, @store[ :seeks, sid] || "\x00\x00\x00\x00\x00\x00\x00\x00", file].pack 'nNa8a*'
-					sock.write [pc.length, pc].pack( 'Na*')
 				end
 			end
 		end
